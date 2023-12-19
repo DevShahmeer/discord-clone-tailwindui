097140f7749ebc4eae6082d1992bfd0cdf8f7b30
shahmeer <theshahmeerfsd@gmail.com>
2023-12-19 21:41:59 +0500

    Added Custom Fonts and Colors

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex h-screen text-white">
-      <div className="p-3 space-y-2 overflow-y-scroll bg-gray-800">
+    <div className="flex h-screen text-gray-100">
+      <div className="p-3 space-y-2 overflow-y-scroll bg-gray-900">
         {[...Array(40)].map((_, i) => (
           <div
             className="flex items-center justify-center w-12 h-12 text-gray-800 bg-white rounded-full"
@@ -12,18 +12,20 @@ export default function Home() {
         ))}
       </div>
 
-      <div className="flex flex-col bg-gray-700 w-60">
-        <div className="flex items-center h-12 px-3 shadow-md">
+      <div className="flex flex-col bg-gray-800 w-60">
+        <div className="flex items-center h-12 px-3 shadow-md text-white font-title font-medium">
           Tailwind CSS
         </div>
-        <div className="flex-1 p-3 space-y-2 overflow-y-scroll">
+        <div className="flex-1 p-3 space-y-2 overflow-y-scroll text-gray-300">
+          <p className="text-white">channel (unread)</p>
+          <p className="text-white">channel (unread)</p>
           {[...Array(40)].map((_, i) => (
             <p key={i}>channel {i}</p>
           ))}
         </div>
       </div>
 
-      <div className="flex flex-col flex-1 bg-gray-600">
+      <div className="flex flex-col flex-1 bg-gray-700">
         <div className="flex items-center h-12 px-3 shadow-md">general</div>
         <div className="flex-1 p-3 space-y-4 overflow-y-scroll">
           {[...Array(40)].map((_, i) => (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,37 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Whitney", "Open Sans", ...defaultTheme.fontFamily.sans],
+        title: ["Ginto", "Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        gray: {
+          50: "#ECEDEE",
+          100: "#DCDDDE",
+          200: "#B9BBBE",
+          300: "#8E9297",
+          400: "#72767D",
+          500: "#5C6067",
+          600: "#464950",
+          700: "#36393F",
+          800: "#2F3136",
+          900: "#202225",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
